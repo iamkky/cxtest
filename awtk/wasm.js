3d8797d785fb3e4303f9653195d121776c89c803
iamkky <iamkky@nowhere>
2023-10-15 22:11:27 -0400

    Objectivating of API functions

--- awtk/wasm.js
+++ awtk/wasm.js
@@ -1,152 +1,60 @@
-var instanceObj;
-var memory;
+//var instanceObj;
+//var memory;
 
-// ========================================== STRING =========================================
-function wasmGetString(instanceObj, memory, ws)
-{
-	len = instanceObj.instance.exports.stringBufferLength_(ws);
-	p   = instanceObj.instance.exports.stringBufferGetBuffer_(ws);
-	uint8 = new Uint8Array(memory.buffer, p, len);
-	return (new TextDecoder()).decode(uint8);
-}
+function awtkModule() {
+	this.module = null;
+	this.instance = null;
+	this.memory = null;
+	this.exports = null;
 
-function wasmStringNew(instanceObj, memory, str)
-{
-	const encoder = new TextEncoder();
-	const view = encoder.encode(str);
+	// Load and instantiate the WebAssembly module
+	this.load = async function(url) {
+		try {
+			this.memory = new WebAssembly.Memory({ initial: 16 });
+			const response = await fetch(url);
+			const buffer = await response.arrayBuffer();
+			this.module = await WebAssembly.compile(buffer);
 
-	len = view.length;
-	ws = instanceObj.instance.exports.StringBufferNew_(len+1);
-	
-	if(ws>0){
-		p = instanceObj.instance.exports.stringBufferGetBuffer_(ws);
-		var uint8 = new Uint8Array(memory.buffer, p, len+1);
-		uint8.set(view);
-		uint8[len] = 0;
-	 	instanceObj.instance.exports.stringBufferHardsetLength_(ws, len+1);
-	}
-	return ws;
-}
+			console.log(WebAssembly.Module.imports(this.module));
 
-function wasmStringFree(instanceObj, str)
-{
-	instanceObj.instance.exports.stringBufferFree_(ws);
-}
-
-// ========================================= CONSOLE =========================================
-
-function consoleLogMsg(str)
-{
-	var msg = wasmGetString(instanceObj, memory, str);
-	console.log(msg);
-}
-
-// ========================================== EVENT ==========================================
-
-function globalHandler(event)
-{
-	const f = event.target.fback;
-	var value = wasmStringNew(instanceObj, memory, f.EV);
-	//console.log("V: "+f.EV+" P:"+f.EP+" C:"+f.EC);
-	instanceObj.instance.exports.globalHandler(f.EP, f.EC, value);
-	instanceObj.instance.exports.StringBufferFree_(value);
-}
-
-function wasmFetchHandler(response, component, handler)
-{
-	var response_ws = wasmStringNew(instanceObj, memory, response);
-	instanceObj.instance.exports.globalFetchHandler2(response_ws, component, handler);	
-	instanceObj.instance.exports.StringBufferFree_(response_ws);
-}
-
-function wasmFetch(url_ws, component, handler)
-{
-	var url = wasmGetString(instanceObj, memory, url_ws);
-	fetch(url).then(response => response.text()).then(response => wasmFetchHandler(response, component, handler));
-}
-
-function fbackRenderWasm(str)
-{
-	console.log(">>>> RENDER >>>>");
-	var newpagejson = wasmGetString(instanceObj, memory, str);
-
-	//console.log(newpagejson);
-        var oldNode = document.getElementById("app");
-
-	fbackStatReset();
-
-        var eltree = JSON.parse(newpagejson);
-	console.log(eltree);
-	var newNode = fbackRenderReplace(0,oldNode, eltree, globalHandler)
-
-	fbackStatLog();
-
-	if(newNode !== oldNode){
-		console.log("Replace");
-        	var par = oldNode.parentNode;
-        	par.replaceChild(newNode, oldNode);
-	}
-}
-
-// ========================================== START  ==========================================
-
-async function startWasm(url)
-{
-	memory = new WebAssembly.Memory({ initial: 16 });
-	const response = await fetch(url);
-	const buffer = await response.arrayBuffer();
-	const module = await WebAssembly.compile(buffer);
-	console.log(WebAssembly.Module.imports(module));
-
-	instanceObj = await WebAssembly.instantiate(buffer, {env: 
+			this.instance = await WebAssembly.instantiate(buffer,
+						{env: 
 							{
-							memory:memory,
-							consoleLog:console.log,
-							fbackRenderWasm:fbackRenderWasm,
-							consoleLogMsg:consoleLogMsg,
-							wasmFetch__:wasmFetch
+							memory:this.memory,
+							//consoleLog:console.log,
+							consoleLog:this.consoleLog,
+							fbackRenderWasm:this.fbackRenderWasm.bind(this),
+							consoleLogMsg:this.consoleLogMsg.bind(this),
+							wasmFetch__:this.wasmFetch.bind(this)
 							}
 						});
 
-	console.log(instanceObj.instance.exports);
+			this.exports = this.instance.instance.exports;
+			console.log(this.exports);
 
-	console.log("Staring");
-	instanceObj.instance.exports.AppInit();
-	console.log("End");
-}
+			if (this.instance) {
+				return this.instance;
+			} else {
+				throw new Error("Failed to instantiate the WebAssembly module.");
+			}
+		} catch (error) {
+			throw new Error("Error loading the WebAssembly module: " + error);
+		}
+	}
 
-async function WasmModule(url)
-{
-	const response = await fetch(url);
-	const buffer = await response.arrayBuffer();
-	const module = await WebAssembly.compile(buffer);
-	console.log(WebAssembly.Module.imports(module));
+	// Execute the internal startUp() function
+	this.startModule = function() {
+		if (this.instance && this.exports && typeof this.exports.startModule === "function") {
+			this.exports.startModule();
+		} else {
+			console.error("startUp() function not found in the WebAssembly module.");
+		}
+	}
 
-	this.memory = new WebAssembly.Memory({ initial: 16 });
-
-	this.instanceObj = await WebAssembly.instantiate(buffer, {env: 
-							{
-							memory:memory,
-							consoleLog:console.log,
-							fbackRenderWasm:fbackRenderWasm,
-							consoleLogMsg:consoleLogMsg,
-							wasmFetch__:wasmFetch
-							}
-						});
-
-	this.exports = this.instanceObj.instance.exports;
-
-	console.log(this.exports);
-
-	console.log("Staring");
-	this.exports.AppInit();
-	console.log("End");
-
-
-	this.wasmGetString = function(instanceObj, memory, ws) {
+	this.wasmGetString = function(ws) {
 		len = this.exports.stringBufferLength_(ws);
 		p   = this.exports.stringBufferGetBuffer_(ws);
-		uint8 = new Uint8Array(memory.buffer, p, len);
+		uint8 = new Uint8Array(this.memory.buffer, p, len);
 		return (new TextDecoder()).decode(uint8);
 	}
 
@@ -159,7 +67,7 @@ async function WasmModule(url)
 	
 		if(ws>0){
 			p = this.exports.stringBufferGetBuffer_(ws);
-			var uint8 = new Uint8Array(this. memory.buffer, p, len+1);
+			var uint8 = new Uint8Array(this.memory.buffer, p, len+1);
 			uint8.set(view);
 			uint8[len] = 0;
 	 		this.exports.stringBufferHardsetLength_(ws, len+1);
@@ -170,5 +78,83 @@ async function WasmModule(url)
 	this.wasmStringFree = function(str) {
 		this.exports.stringBufferFree_(ws);
 	}
+
+	this.consoleLogMsg =  function(str)
+	{
+		var msg = this.wasmGetString(str);
+		console.log(msg);
+	}
+
+	this.globalHandler = function(event)
+	{
+		const f = event.target.fback;
+		var value = this.wasmStringNew(f.EV);
+		//console.log("V: "+f.EV+" P:"+f.EP+" C:"+f.EC);
+		this.exports.globalHandler(f.EP, f.EC, value);
+		this.exports.StringBufferFree_(value);
+	}
+
+
+	this.wasmFetchHandler = function(response, component, handler)
+	{
+		var response_ws = this.wasmStringNew(response);
+		this.exports.globalFetchHandler2(response_ws, component, handler);	
+		this.exports.StringBufferFree_(response_ws);
+	}
+
+
+	this.wasmFetch = function(url_ws, component, handler)
+	{
+		var url = this.wasmGetString(url_ws);
+		// not tested, i think the bind is needed.. But not tested
+		fetch(url).then(response => response.text()).then(response => this.wasmFetchHandler(response, component, handler).bind(this));
+	}
+
+
+	this.fbackRenderWasm = function(str)
+	{
+		console.log(">>>> RENDER >>>>");
+		var newpagejson = this.wasmGetString(str);
+
+		//console.log(newpagejson);
+	       	 var oldNode = document.getElementById("app");
+
+		fbackStatReset();
+
+       		var eltree = JSON.parse(newpagejson);
+		console.log(eltree);
+		var newNode = fbackRenderReplace(0,oldNode, eltree, this.globalHandler.bind(this))
+
+		fbackStatLog();
+
+		if(newNode !== oldNode){
+			console.log("Replace");
+       	 		var par = oldNode.parentNode;
+       	 		par.replaceChild(newNode, oldNode);
+		}
+	}
 }
 
+async function startWasm(url)
+{
+	console.log("Starting Wasm");
+	myAwtkModule = new awtkModule();
+	
+	await myAwtkModule.load(url)
+
+	console.log("Staring");
+	myAwtkModule.exports.AppInit();
+	console.log("End");
+}
+
+/*
+const myWasmModule = new WasmModule();
+myWasmModule.load('example.wasm')
+	.then(instance => {
+		console.log('WebAssembly module loaded', instance);
+		myWasmModule.startUp();
+	})
+	.catch(error => {
+		console.error(error);
+	});
+*/
